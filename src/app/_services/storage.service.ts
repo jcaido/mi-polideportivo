b3b35ea623bb13e04dea202313482972dcfb77dc
Cristobal Rosa <crisrogar@gmail.com>
2024-06-28 10:29:59 +0200

    create method clean in storage.service

---
+++ src/app/_services/storage.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+const USER_KEY = 'auth-user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor() { }
+
+  clean() :void {
+    window.sessionStorage.clear();
+  }
+}
