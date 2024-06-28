3e85120bcef210d422578fbe627096fdb3c0acb7
Cristobal Rosa <crisrogar@gmail.com>
2024-06-28 10:37:52 +0200

    creat method gerUser in storage.service

--- src/app/_services/storage.service.ts
+++ src/app/_services/storage.service.ts
@@ -17,4 +17,12 @@ export class StorageService {
     window.sessionStorage.removeItem(USER_KEY);
     window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
   }
+
+  public getUser() :any {
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if(user) {
+      return JSON.parse(user);
+    }
+    return {};
+  }
 }
