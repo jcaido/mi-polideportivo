94c436bb9bb74b85bd17639330291cd3c450d531
Cristobal Rosa <crisrogar@gmail.com>
2024-07-02 19:54:58 +0200

    update login component, login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { StorageService } from '../_services/storage.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  isLoggedIn: boolean = false;
+  isLoginFailed: boolean = false;
+  errorMessage: string = '';
+  roles: string[] = [];
+
+  constructor(private authService: AuthService, private storageService: StorageService, private fb :FormBuilder) {}
+
+  formularioLogin = this.fb.group({
+    nombre: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  ngOnInit(): void {
+    if(this.storageService.isLoggedIn()) {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit() :void {
+    this.authService.login(
+      this.formularioLogin.value.nombre!,
+      this.formularioLogin.value.password!
+    ).subscribe({
+      next: data => {
+        this.storageService.saveUser(data);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        this.reloadPage();
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    })
+  }
+
+  reloadPage() :void {
+    window.location.reload();
+  }
 
 }
