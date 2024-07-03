320cde6591f38eae9eeebc88e160ef1864cb5397
Cristobal Rosa <crisrogar@gmail.com>
2024-07-03 13:25:19 +0200

    create method logout in navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(private authService: AuthService, private storageService: StorageService) {}
+
+  logout() :void {
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+        window.location.reload();
+      },
+      error: err => {
+        console.error(err);
+      }
+    });
+  }
 
 }
