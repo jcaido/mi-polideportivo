6527445b366acd0d12db0b9e55ad283cdbede287
Cristobal Rosa <crisrogar@gmail.com>
2024-07-04 20:33:02 +0200

    create method getAllFacilities in facility.service

--- src/app/_services/facility.service.ts
+++ src/app/_services/facility.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const FACILITY_API = 'http://localhost:8080/api/facility/';
+
+const httOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class FacilityService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllFacilities() :Observable<any> {
+    return this.http.get(FACILITY_API + "facilities", httOptions);
+  }
 }
