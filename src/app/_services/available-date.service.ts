5f462906eee12ee89d4dcb3784e46fa9e6e4f811
Cristobal Rosa <crisrogar@gmail.com>
2024-07-08 19:16:36 +0200

    create method getAvailableDatesByFacility in available date service

--- src/app/_services/available-date.service.ts
+++ src/app/_services/available-date.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const AVAILABLE_DATE_API = 'http://localhost:8080/api/available-date/dates/';
+
+const httOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AvailableDateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAvailableDatesByFacility(id_facility: number): Observable<any> {
+    return this.http.get(`AVAILABLE_DATE_API${id_facility}`, httOptions);
+  }
 }
