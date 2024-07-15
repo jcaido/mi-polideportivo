92839113d8ef49be614b55a0cebc186993ccfc37
Cristobal Rosa <crisrogar@gmail.com>
2024-07-15 11:33:03 +0200

    update calendar component

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { AvailableDateService } from '../_services/available-date.service';
-import { Subject } from 'rxjs';
+import { TimeBookService } from '../_services/time-book-service';
+import { TimeBookAvailable } from '../_helpers/timeBookAvailable';
 
 @Component({
   selector: 'app-calendar',
@@ -20,8 +21,8 @@ export class CalendarComponent implements OnInit{
   date: any;
 
   availableDates: Date[] = [
-    new Date(2024, 6, 3),
-    new Date(2024, 6, 12),
+    new Date(2024, 6, 8),
+    new Date(2024, 6, 15),
     new Date(2024, 6, 20),
   ];
 
@@ -30,8 +31,9 @@ export class CalendarComponent implements OnInit{
   dateAPI: string[] = [];
   dateModify :string = "";
 
+  timeBookAvailable!: TimeBookAvailable;
 
-  constructor(private availableDateService: AvailableDateService) {}
+  constructor(private availableDateService: AvailableDateService, private timeBookService: TimeBookService) {}
 
   ngOnInit(): void {
     this.availableDateService.getAvailableDatesByFacility(this.idFacility!).subscribe(dates => {
@@ -47,6 +49,12 @@ export class CalendarComponent implements OnInit{
       this.dateAPI = this.date.split("/");
       this.dateModify = this.dateAPI[2] + "-" + this.monthDayModify(this.dateAPI[1]) + "-" + this.monthDayModify(this.dateAPI[0]);
       console.log(this.dateModify);
+      this.timeBookService.getAvailableDateTimeByAvailableDateAndFacility(this.dateModify, this.idFacility!).subscribe(data => {
+        this.timeBookAvailable = data;
+        console.log(this.dateModify);
+        console.log(this.idFacility);
+        console.log(this.timeBookAvailable);
+      });
     }
     return this.date;
   }
