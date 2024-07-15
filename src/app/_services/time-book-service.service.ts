9668fc411477f9bc2a919a7169e69d1da34e798f
Cristobal Rosa <crisrogar@gmail.com>
2024-07-15 11:11:17 +0200

    update timeBook service

--- src/app/_services/time-book-service.service.ts
+++ src/app/_services/time-book-service.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TimeBookAvailable } from '../_helpers/timeBookAvailable';
+
+const TIMEBOOK_API = 'http://localhost:8080/api/available-date-time/';
+
+const httOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimeBookServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAvailableDateTimeByAvailableDateAndFacility(date: string, id_facility: number): Observable<any> {
+    return this.http.get<TimeBookAvailable>(TIMEBOOK_API + "available-times/" + `${date}` + "/" + `${id_facility}`, httOptions)
+  }
 }
