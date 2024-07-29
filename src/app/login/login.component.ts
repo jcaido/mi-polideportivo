b18d752a2960d7c2bb5fd918cb1b4e8651bb2c4b
Cristobal Rosa <crisrogar@gmail.com>
2024-07-29 13:23:37 +0200

    update login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { StorageService } from '../_services/storage.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +13,10 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit{
 
   isLoggedIn: boolean = false;
-  isLoginFailed: boolean = false;
   errorMessage: string = '';
   roles: string[] = [];
 
-  constructor(private authService: AuthService, private storageService: StorageService, private fb :FormBuilder) {}
+  constructor(private authService: AuthService, private storageService: StorageService, private fb :FormBuilder,private _snackBar: MatSnackBar, private router: Router) {}
 
   formularioLogin = this.fb.group({
     nombre: ['', Validators.required],
@@ -36,20 +37,17 @@ export class LoginComponent implements OnInit{
     ).subscribe({
       next: data => {
         this.storageService.saveUser(data);
-        this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.roles = this.storageService.getUser().roles;
-        this.reloadPage();
+        this._snackBar.open("Sesión iniciada con rol/es "+ this.roles, "Cerrar");
+        this.router.navigate(['/information']);
       },
       error: err => {
         this.errorMessage = err.error.message;
-        this.isLoginFailed = true;
+        this._snackBar.open(this.errorMessage, "Cerrar", {
+          duration: 5000
+        });
       }
     })
   }
-
-  reloadPage() :void {
-    window.location.reload();
-  }
-
 }
