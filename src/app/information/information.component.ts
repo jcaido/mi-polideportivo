6659d4e46656eef3bdb6f61c26d919053f3bdfd4
Cristobal Rosa <crisrogar@gmail.com>
2024-08-18 11:22:46 +0200

    update information component

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-information',
   templateUrl: './information.component.html',
   styleUrl: './information.component.css'
 })
-export class InformationComponent {
+export class InformationComponent implements OnInit {
 
+  username: string = '';
+
+  constructor(private storageService: StorageService) {
+    this.username = storageService.getUser().username;
+  }
+
+  ngOnInit(): void {
+    if (!sessionStorage.getItem('pageReloaded')) {
+      // Marca la página como recargada
+      sessionStorage.setItem('pageReloaded', 'true');
+      // Recargar la página
+      window.location.reload();
+    } else {
+      // Limpia el estado para futuras recargas
+      sessionStorage.removeItem('pageReloaded');
+    }
+  }
 }
