a34e3bcb79eebff2c7788063c78f3ba1bfc5d936
Cristobal Rosa <crisrogar@gmail.com>
2024-08-19 11:10:20 +0200

    create method pay, payment service

--- src/app/payment/payment.service.ts
+++ src/app/payment/payment.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { PaymentIntentDto } from './payment-intent-dto';
+import { Observable } from 'rxjs';
+
+const PAYMENT_API = 'http://localhost:8080/api/payment/';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public pay(paymentIntentDto: PaymentIntentDto): Observable<string> {
+    return this.httpClient.post<string>(PAYMENT_API + 'paymentIntent', paymentIntentDto, httpOptions);
+  }
 }
