7576a41a4085686a848ae694906776837fcb2a5f
Cristobal Rosa <crisrogar@gmail.com>
2024-08-26 12:35:57 +0200

    update confirm method in payment seervice

--- src/app/payment/payment.service.ts
+++ src/app/payment/payment.service.ts
@@ -20,8 +20,8 @@ export class PaymentService {
     return this.httpClient.post<string>(PAYMENT_API + 'paymentIntent', paymentIntentDto, httpOptions);
   }
 
-  public confirm(id: string): Observable<string> {
-    return this.httpClient.post<string>(PAYMENT_API + `confirm/${id}`, {}, httpOptions);
+  public confirm(id: string, id_available_date_time: string, id_user: string): Observable<string> {
+    return this.httpClient.post<string>(PAYMENT_API + `confirm/${id}/${id_available_date_time}/${id_user}`, {}, httpOptions);
   }
 
   public cancel(id: string): Observable<string> {
