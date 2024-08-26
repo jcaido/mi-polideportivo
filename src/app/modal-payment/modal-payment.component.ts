0a183ce3bca5ab6eb5576339c44558e6fca428a9
Cristobal Rosa <crisrogar@gmail.com>
2024-08-26 12:38:32 +0200

    update modal payment component

--- src/app/modal-payment/modal-payment.component.ts
+++ src/app/modal-payment/modal-payment.component.ts
@@ -1,23 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { PaymentService } from '../payment/payment.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { StorageService } from '../_services/storage.service';
 
 @Component({
   selector: 'app-modal-payment',
   templateUrl: './modal-payment.component.html',
   styleUrl: './modal-payment.component.css'
 })
-export class ModalPaymentComponent {
+export class ModalPaymentComponent implements OnInit {
 
   readonly dialogRef = inject(MatDialogRef<ModalPaymentComponent>);
   readonly data = inject(MAT_DIALOG_DATA);
 
-  constructor(private paymentService: PaymentService, private _snackBar: MatSnackBar, private route: Router) {}
+  idUser: string = '';
 
-  confirm(id: string) {
-    this.paymentService.confirm(id).subscribe(
+  constructor(private paymentService: PaymentService, private _snackBar: MatSnackBar, private route: Router, private storageService: StorageService) {}
+
+  ngOnInit(): void {
+    this.idUser = this.storageService.getUser().id;
+  }
+
+  confirm(id: string, id_available_date_time: string, id_user: string) {
+    this.paymentService.confirm(id, id_available_date_time, id_user).subscribe(
       data => {
         this.dialogRef.close();
         this._snackBar.open("Pago confirmado", "Cerrar", {
