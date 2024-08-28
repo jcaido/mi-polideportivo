7532e8e03d2cf325cad501ab986734de0476b515
Cristobal Rosa <crisrogar@gmail.com>
2024-08-28 19:09:56 +0200

    update my-booking-component

--- src/app/my-bookings/my-bookings.component.ts
+++ src/app/my-bookings/my-bookings.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BooksByUser } from '../_helpers/bookByUser';
+import { StorageService } from '../_services/storage.service';
+import { BooksService } from '../_services/books.service';
 
 @Component({
   selector: 'app-my-bookings',
   templateUrl: './my-bookings.component.html',
   styleUrl: './my-bookings.component.css'
 })
-export class MyBookingsComponent {
+export class MyBookingsComponent implements OnInit {
+
+  booksByUser: BooksByUser[] = [];
+  idUser: string = '';
+  errorMessage: string = '';
+
+  constructor(private storageService: StorageService, private booksService: BooksService) {}
+
+  ngOnInit(): void {
+    this.idUser = this.storageService.getUser().id;
+    this.booksService.getAllBooksByUser(this.idUser).subscribe({
+      next: data => {
+        this.booksByUser = data;
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+      }
+    })
+  }
 
 }
