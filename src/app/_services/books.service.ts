90cf02e22c780a7ad1557fdfc7e12dd5b0f55c71
Cristobal Rosa <crisrogar@gmail.com>
2024-08-28 19:08:42 +0200

    create  method getAllBooksByUser in books service

--- src/app/_services/books.service.ts
+++ src/app/_services/books.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BOOKS_API = 'http://localhost:8080/api/books/';
+
+const httOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getAllBooksByUser(id_user: string): Observable<any> {
+    return this.httpClient.get(BOOKS_API + `books_by_user/${id_user}`, httOptions);
+  }
 }
