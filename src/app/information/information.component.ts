4c06c8b5f0091607e89d60a866b87cc64de61d83
Cristobal Rosa <crisrogar@gmail.com>
2024-09-11 14:13:07 +0200

    update information component

--- src/app/information/information.component.ts
+++ src/app/information/information.component.ts
@@ -10,15 +10,5 @@ export class InformationComponent implements OnInit {
 
   constructor(private storageService: StorageService) {}
 
-  ngOnInit(): void {
-    if (!sessionStorage.getItem('pageReloaded')) {
-      // Marca la página como recargada
-      sessionStorage.setItem('pageReloaded', 'true');
-      // Recargar la página
-      window.location.reload();
-    } else {
-      // Limpia el estado para futuras recargas
-      sessionStorage.removeItem('pageReloaded');
-    }
-  }
+  ngOnInit(): void {}
 }
